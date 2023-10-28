2cab740e0940001c6568eef1d04d8471f9fe0a61
ssulakkhana <ssulakkhana77@gmail.com>
2023-10-28 23:50:48 +0530

    install swagger-ui-express

--- server/server.js
+++ server/server.js
@@ -2,12 +2,24 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000; // Define the port
 
+const yaml = require('js-yaml');
+const fs = require('fs');
+
+// Load your Swagger YAML file
+const swaggerFile = 'D:/pasinduJayalal/openapi.yaml';
+
+const swaggerYaml = yaml.load(fs.readFileSync(swaggerFile, 'utf8'));
+
 // Middleware to parse JSON requests
 app.use(express.json());
 
+// Serve the Swagger UI
+const swaggerUi = require('swagger-ui-express');
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerYaml));
+
 // Define a route
 app.get('/', (req, res) => {
-    res.send('Hello, Express!');
+    res.send('Hello, Express');
 });
 
 // Start the server
